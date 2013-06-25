f4a3069069867aaf9b8c958967e5b62562f3506b
Nana Kwame <nanakwame123@gmail.com>
2013-06-25 07:46:13 +0000

    MusicManager changed to SongManager and implements LinkedHashMaps instead of HashMaps

---
+++ lurvberry/src/com/example/lurvberry/SongManager.java
@@ -0,0 +1,42 @@
+package com.example.lurvberry;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+import android.os.Environment;
+
+public class SongManager {
+	// SDCard Path
+	final String MEDIA_PATH = Environment.getExternalStorageDirectory().getPath();
+	private ArrayList<LinkedHashMap<String, String>> songsList = new ArrayList<LinkedHashMap<String, String>>();
+	
+	// Constructor
+	public SongManager(){
+		
+	}
+	
+	/* Function to read all mp3 files from sdcard and store the details in ArrayList */
+	public ArrayList<LinkedHashMap<String, String>> getSongs(){
+		File home = new File(MEDIA_PATH);
+
+		if (home.listFiles(new FileExtensionFilter()).length > 0) {
+			for (File file : home.listFiles(new FileExtensionFilter())) {
+				LinkedHashMap<String, String> song = new LinkedHashMap<String, String>();
+				song.put("songTitle", file.getName().substring(0, (file.getName().length() - 4)));
+				song.put("songPath", file.getPath());
+				
+				songsList.add(song); // Adding each song to SongList
+			}
+		}
+		return songsList; // return songs list array
+	}
+	
+	/* Class to filter files which are having .mp3 extension */
+	class FileExtensionFilter implements FilenameFilter {
+		public boolean accept(File dir, String name) {
+			return (name.endsWith(".mp3") || name.endsWith(".MP3"));
+		}
+	}
+}
